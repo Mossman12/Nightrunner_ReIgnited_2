f32897e9df13aa134c0f054873fd3bb262467623
Moses McKeon <moses@maaac.ca>
2024-03-10 22:35:35 -0400

    Fixed missing files

---
+++ Plugins/SubtitleHookUMG/Source/SubtitleHookUMG/Private/SubtitleHookUMG.cpp
@@ -0,0 +1,17 @@
+// Copyright Daniel Orchard 2021. All Rights Reserved.
+
+#include "SubtitleHookUMG.h"
+
+#define LOCTEXT_NAMESPACE "FSubtitleHookUMGModule"
+
+void FSubtitleHookUMGModule::StartupModule()
+{
+}
+
+void FSubtitleHookUMGModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FSubtitleHookUMGModule, SubtitleHookUMG)
